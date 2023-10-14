5b8c2ac4c6159793172422812214dba540e0f24f
carlosp81 <gtrujilloc48@gmail.com>
2023-10-14 18:18:18 -0500

    feat(front) : Add https support to Angular app in Dev mode
    Install mkcert in the system and install cerficate in the browser

--- Dating-WebAPI/Program.cs
+++ Dating-WebAPI/Program.cs
@@ -29,7 +29,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(builder => builder.AllowAnyHeader()
-.AllowAnyMethod().WithOrigins("http://localhost:4200"));
+.AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
