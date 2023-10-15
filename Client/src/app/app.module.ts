eaaabffd68dd2dfdd34aebe49865730f8c88401d
carlosp81 <gtrujilloc48@gmail.com>
2023-10-15 08:04:55 -0500

    feat: Send Login's Data to the server
    Currently the login is Template-Driven form

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Client/src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit{
+  model: any = {};
+  loggedIn: boolean = false;
+  
+  constructor(private accountService: AccountService){}
+
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response)
+        this.loggedIn = true;
+      },
+      error: error => console.error(error)      
+    });
+  }
+
+  logout(){
+    this.loggedIn = false;
+  }
+
+
+}
