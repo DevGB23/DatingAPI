a51c0f41091da3e9f86fe72021ddfb6b088bd1e9
carlosp81 <gtrujilloc48@gmail.com>
2023-10-19 20:27:56 -0500

    feat(front): Implement like's User feature in the Client

--- Client/src/app/_services/members.service.ts
+++ Client/src/app/_services/members.service.ts
@@ -103,6 +103,16 @@ export class MembersService {
   deletePhoto(photoId: number) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
+  
+  
+  addLike(username: string) {
+    return this.http.post(this.baseUrl + 'likes/' + username, {})
+  }
+
+  
+  getLikes(predicate: string) {
+    return this.http.get<Member[]>(this.baseUrl + 'likes?predicate=' + predicate, {});
+  }
 
 
   private getPaginatedresults<T> (url: string, params: HttpParams) {
--- Client/src/app/lists/lists.component.ts
+++ Client/src/app/lists/lists.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../_models/member';
+import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.css']
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit{
+  members: Member[] = [];
+  predicate = 'liked';
+
+  constructor(private memberService: MembersService){}
+
+  ngOnInit(): void {
+    this.loadLikes()
+  }
+
+  loadLikes () {
+    this.memberService.getLikes(this.predicate).subscribe({
+      next: response => {
+        this.members = response;
+      }
+    })
+  }
 
 }
