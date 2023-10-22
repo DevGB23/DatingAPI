052f3be757ae06482eaa7a2456b4410bb2eb16ab
carlosp81 <gtrujilloc48@gmail.com>
2023-10-21 23:43:50 -0500

    feat: Add a policy based Authorization

--- Dating-WebAPI/Extensions/IdentityServiceExtensions.cs
+++ Dating-WebAPI/Extensions/IdentityServiceExtensions.cs
@@ -34,6 +34,13 @@ public static class IdentityServiceExtensions
                 };
         });
 
+
+        services.AddAuthorization(opt => 
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+
         return services;
     }
 }
